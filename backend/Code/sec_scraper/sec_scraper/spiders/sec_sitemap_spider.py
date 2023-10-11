78a436f0849ac4983060e71e8b2b9a6c2121ad55
unknown <IFlearnandwork@protonmail.com>
2023-10-11 20:48:53 +0300

    starting the parse logic

--- backend/Code/sec_scraper/sec_scraper/spiders/sec_sitemap_spider.py
+++ backend/Code/sec_scraper/sec_scraper/spiders/sec_sitemap_spider.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import scrapy
 import requests
 import pandas as pd
 
@@ -14,23 +15,23 @@ from scrapy.http import HtmlResponse
 import psycopg2
 from selenium import webdriver
 import xml.etree.ElementTree as ET
+
 #database values
-conn=psycopg2.connect(database="postgres",
-                      user="postgres",
-                      password="If2722002!",
-                      port="5432"
-                      )
 
 
 class SecSitemapSpider(Spider):
     name = 'SecSitemapSpider'
     start_urls = ['https://www.sec.gov/sitemap']
-
+  
     def parse(self, response):
         try:
             # Select the table containing the links
             table = response.xpath('//table[@class="sitemap"]')
-
+            conn=psycopg2.connect(database="postgres",
+                                    user="postgres",
+                                    password="If2722002!",
+                                    port="5432"
+                                    )
             # Select all the links within the table
             links = table.xpath('.//a/@href').getall()
             #implementing the cursor
@@ -52,65 +53,87 @@ class SecSitemapSpider(Spider):
 
 
 class SecInsiderTradesSpider(Spider):
-        name = "sec_insider_trades"
-        allowed_domains = ["sec.gov"]
-        start_urls = ["http://www.sec.gov"]
-        cik = None
-        driver = None
+    name = "sec_insider_trades"
+    allowed_domains = ["sec.gov"]
+    start_urls = ["http://www.sec.gov"]
+    cik = None
+    driver = None
 
-        def __init__(self, cik=None, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.cik = cik
-            options = FirefoxOptions()
-            options.add_argument('--headless')
-            driver = webdriver.Firefox(options=options)
-            self.driver = driver
+    def __init__(self, cik=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cik = cik
+        # Configure Firefox options
+        options = FirefoxOptions()
+        # Uncomment the line below to run the browser in headless mode
+        # options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
+        self.driver = driver
 
-        @classmethod
-        def from_crawler(cls, crawler, *args, **kwargs):
-            spider = super(SecInsiderTradesSpider, cls).from_crawler(crawler, *args, **kwargs)
-            crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
-            return spider
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        # Connect the spider_closed signal to spider_closed method
+        spider = super(SecInsiderTradesSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
 
-        def start_requests(self):
-            try:
-                url = f"https://www.sec.gov/edgar/browse/?CIK={self.cik}&owner=include"
-                self.driver.get(url)
-                element = self.driver.find_element(By.XPATH, '//*[@id="filingsStart"]/div[2]/div[5]/div')
-                self.driver.execute_script('arguments[0].style.display="block"', element)
-                element = WebDriverWait(self.driver, 50).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="filingsStart"]/div[2]/div[5]/div/button[1]')))
-                element.click()
+    def start_requests(self):
+        try:
+            # Construct URL with CIK and open it in the browser
+            url = f"https://www.sec.gov/edgar/browse/?CIK={self.cik}&owner=include"
+            self.driver.get(url)
+            
+            # Perform some actions on the page using Selenium
+            element = self.driver.find_element(By.XPATH, '//*[@id="filingsStart"]/div[2]/div[5]/div')
+            self.driver.execute_script('arguments[0].style.display="block"', element)
+            element = WebDriverWait(self.driver, 50).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="filingsStart"]/div[2]/div[5]/div/button[1]')))
+            element.click()
 
-                body = self.driver.page_source.encode("utf-8")
-                response = HtmlResponse(url=url, body=body, encoding="utf-8")
+            # Get the page source and create a Scrapy response
+            body = self.driver.page_source.encode("utf-8")
+            response = HtmlResponse(url=url, body=body, encoding="utf-8")
 
-                yield from self.parse(response)
+            # Yield from the parse method to handle the response
+            yield from self.parse(response)
 
-            except Exception as e:
-                logging.exception("Error occurred during start_requests: %s", str(e))
-                raise CloseSpider("Error occurred during start_requests")
+        except Exception as e:
+            # Log and handle exceptions during start_requests
+            logging.exception("Error occurred during start_requests: %s", str(e))
+            raise CloseSpider("Error occurred during start_requests")
 
-        def parse(self, response):
-            try:
-                header_element = self.driver.find_element(By.XPATH, '//*[@id="filingsTable_wrapper"]/div[3]/div[1]/div/table/thead/tr')
-                headings = [heading.text.replace("\n", " ") for heading in header_element.find_elements(By.TAG_NAME, "th")]
-                table_body = self.driver.find_element(By.XPATH, '//*[@id="filingsTable"]/tbody')
-                rows = table_body.find_elements(By.TAG_NAME, "tr")
-                links = []
-                for row in rows:
-                    cells = row.find_elements(By.TAG_NAME, "td")
-                    form_description_div = cells[1].find_element(By.CSS_SELECTOR, "div[data-export]")
-                    form_description_link = form_description_div.find_element(By.CSS_SELECTOR, "a.document-link")
-                    form_description_href = form_description_link.get_attribute("href")
-                    links.append(form_description_href)
-            except Exception as e:
-                logging.exception("Error occurred during parsing: %s", str(e))
-                raise CloseSpider("Error occurred during parsing")
-            yield {"links": links}
-        def spider_closed(self, spider):
-                    if hasattr(spider, 'driver'):
-                        spider.driver.quit()
+    def parse(self, response):
+        try:
+            # Extract table headings
+            header_element = self.driver.find_element(By.XPATH, '//*[@id="filingsTable_wrapper"]/div[3]/div[1]/div/table/thead/tr')
+            headings = [heading.text.replace("\n", " ") for heading in header_element.find_elements(By.TAG_NAME, "th")]
+
+            # Extract links from the table
+            table_body = self.driver.find_element(By.XPATH, '//*[@id="filingsTable"]/tbody')
+            rows = table_body.find_elements(By.TAG_NAME, "tr")
+            links = []
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                form_description_div = cells[1].find_element(By.CSS_SELECTOR, "div[data-export]")
+                form_description_link = form_description_div.find_element(By.CSS_SELECTOR, "a.document-link")
+                form_description_href = form_description_link.get_attribute("href")
+                links.append(form_description_href)
+        except Exception as e:
+            # Log and handle exceptions during parsing
+            logging.exception("Error occurred during parsing: %s", str(e))
+            raise CloseSpider("Error occurred during parsing")
+        
+        # Yield the extracted links
+        for link in links:
+            yield scrapy.Request(url=link,callback=self.parse_link)
+    
+    #parsing the links
+    def parse_link(self,response):
+        self.driver.get(response.url)
+    
+    def spider_closed(self, spider):
+        # Quit the browser when the spider is closed
+        if hasattr(spider, 'driver'):
+            spider.driver.quit()
 
 class ProcessLinksSpider(Spider):
     name = "process_links"
