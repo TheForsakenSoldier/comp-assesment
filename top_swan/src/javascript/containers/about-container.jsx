93538a27d3d8d23ff31557fc1c65ebf28387fb2b
unknown <IFlearnandwork@protonmail.com>
2023-12-26 09:37:51 +0200

    removing the topswan folder

--- top_swan/src/javascript/containers/about-container.jsx
+++
@@ -1,8 +0,0 @@
-// javascript/containers/about-container.jsx
-import React from 'react';
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <h1>Hello, react!</h1>,
-  document.getElementById('root')
-);
