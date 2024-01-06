89d5de0352878989df0c5aae0889a8be2be0ec46
ForsakenSoldier <IFlearnandwork@protonmail.com>
2024-01-06 17:11:06 +0200

    fixing constant calls

--- backend/Code/rest.api.py
+++ backend/Code/rest.api.py
@@ -6,13 +6,13 @@ import plotly.express as px
 from app import get_financial_data_by_ticker # Importing a function to fetch financial data by ticker symbol
 
 # Initialize the Dash application
-app = Dash(__name__, title="Financial Data analysis", assets_folder="./src") # Setting up the Dash application with a title and specifying the folder for static files
+app = Dash(__name__, title="Top Swan", assets_folder="./web/assets") # Setting up the Dash application with a title and specifying the folder for static files
 
 # Define the layout of the Dash application
 app.layout = html.Div([
  html.Div(className='pre-select-company', children=[ # Creating a div for pre-selection of company
    dcc.Input( id="ticker-search", type="text", placeholder="Enter Company Ticker Here"), # Text input field for entering company ticker
-   html.Button("Search", id="submit-button", n_clicks=None) # Button to submit the entered ticker
+   html.Button("Search", id="submit-button", n_clicks=0) # Button to submit the entered ticker
  ]),
  html.Div(id='post-select-company-div', style={'display':'none'}, children=[ # Creating a hidden div for post-selection of company
    html.H1(id='post-select-company-title'), # Heading for the selected company
@@ -29,25 +29,26 @@ app.layout = html.Div([
 ])
 
 @app.callback(
-  Output('post-select-company-dropdown', 'options'),
-  Input('ticker-search', 'value'))
-def update_dropdown_options(ticker): # Function to update dropdown options based on the entered ticker
-  if ticker is None:
-      raise PreventUpdate # If no ticker is entered, prevent the update
-  else:
-      df = get_financial_data_by_ticker(ticker=ticker) # Fetch the financial data for the entered ticker
-      if df is None:
-          raise PreventUpdate # If no data is fetched, prevent the update
-      else:
-          options = df['label'].unique() # Get unique labels from the fetched data
-          return options # Return the unique labels as options for the dropdown
+ Output('post-select-company-dropdown', 'options'),
+ Input('submit-button', 'n_clicks'),
+ State('ticker-search', 'value'))
+def update_dropdown_options(n_clicks, ticker): 
+ if n_clicks is None or n_clicks <= 0:
+     raise PreventUpdate 
+ else:
+     df = get_financial_data_by_ticker(ticker=ticker) 
+     if df is None:
+         raise PreventUpdate 
+     else:
+         options = df['label'].unique() 
+         return options 
 
 @app.callback(
   Output('post-select-company-div', 'style'),
   Input('submit-button', 'n_clicks'),
   State('post-select-company-div', 'style'))
 def show_div(n_clicks, style): # Function to show the div containing the dropdown after the search button is clicked
-  if n_clicks is not None:
+  if n_clicks >0:
       return {'display': 'block'} # If the search button is clicked, display the div
   else:
       return style # Otherwise, keep the current style of the div
